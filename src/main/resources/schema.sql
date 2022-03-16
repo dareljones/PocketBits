b7daf23ce7118eb16a1f895cdce93d464d5fd012
dareljones <46834627+dareljones@users.noreply.github.com>
2022-03-16 22:36:19 +0530

    removec schema.sql

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE coin_balance(
-                         id int primary key AUTO_INCREMENT,
-                         coin varchar(255) NOT NULL,
-                         balance int,
-                         user_id varchar(255),
-                         created_at varchar(255),
-                         updated_at varchar(255)
-);
